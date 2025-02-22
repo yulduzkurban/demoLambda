89fc69947f4ed05377fcfb8ad0b449f6af21f75f
yulduzkurban <yulduzkurban@gmail.com>
2025-02-22 14:17:00 +0000

    loremipsum

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -2,11 +2,13 @@ package com.task04;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import com.syndicate.deployment.annotations.events.SnsEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.RegionScope;
+import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @LambdaHandler(
     lambdaName = "sns_handler",
@@ -15,13 +17,18 @@ import java.util.Map;
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-public class SnsHandler implements RequestHandler<Object, Map<String, Object>> {
+@SnsEventSource(
+		targetTopic = "lambda_topic",
+		regionScope  = RegionScope.DEFAULT
+)
+@DependsOn(
+		name = "lambda_topic",
+		resourceType = ResourceType.SNS_TOPIC
+)
+public class SnsHandler implements RequestHandler<SNSEvent, Void> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
-		return resultMap;
+	public Void handleRequest(SNSEvent event, Context context) {
+		context.getLogger().log(event.toString());
+		return null;
 	}
 }
