de1a4dfff2bf6a029b598d3177aef338e38d97de
yulduzkurban <yulduzkurban@gmail.com>
2025-02-24 15:27:00 +0000

    task_loremipsumdale

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -2,66 +2,54 @@ package com.task08;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.openmeteo.sdk.WeatherApiResponse;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
-import com.syndicate.deployment.model.Architecture;
-import com.syndicate.deployment.model.ArtifactExtension;
-import com.syndicate.deployment.model.DeploymentRuntime;
-import com.syndicate.deployment.model.RetentionSetting;
-import com.syndicate.deployment.model.lambda.url.AuthType;
-import com.syndicate.deployment.model.lambda.url.InvokeMode;
+import com.task08.layer.OpenMeteoClient;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(
-    lambdaName = "api_handler",
-	roleName = "api_handler-role",
-	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}",
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED,
-	layers = {"sdk-layer"}
+		lambdaName = "api_handler",
+		roleName = "api_handler-role",
+		layers = {"weatherClient"}
 )
 @LambdaLayer(
-		layerName = "sdk-layer",
-		libraries = {"lib/flatbuffers-java-24.3.25.jar", "lib/sdk-1.10.0.jar"},
-		runtime = DeploymentRuntime.JAVA11,
-		architectures = {Architecture.ARM64},
-		artifactExtension = ArtifactExtension.ZIP
+		layerName = "weatherClient",
+		libraries = {"lib/OpenMeteoClient.jar", "lib/commons-lang3-3.14.0.jar"}
 )
-@LambdaUrlConfig(
-		authType = AuthType.NONE,
-		invokeMode = InvokeMode.BUFFERED
-)
-public class ApiHandler implements RequestHandler<Object, String> {
+@LambdaUrlConfig
+public class ApiHandler implements RequestHandler<Object, Map<String, Object>> {
 
-	private static final HttpClient httpClient = HttpClient.newHttpClient();
-	private static final String WEATHER_URL = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m";
+	private static final OpenMeteoClient weatherClient = new OpenMeteoClient();
 
-	public String handleRequest(Object request, Context context) {
-        try {
-            HttpRequest httpRequest = HttpRequest.newBuilder()
-                    .GET()
-                    .uri(new URI(WEATHER_URL))
-                .build();
-			HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-			context.getLogger().log(httpResponse.body());
-//			ObjectMapper objectMapper = new ObjectMapper();
-//			WeatherApiResponse weatherApiResponse = objectMapper.readValue(httpResponse.body(), WeatherApiResponse.class);
-//			context.getLogger().log(weatherApiResponse.toString());
-//			return weatherApiResponse.toString();
-			return httpResponse.body();
-        } catch (URISyntaxException | IOException | InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+	public Map<String, Object> handleRequest(Object request, Context context) {
+
+		if (StringUtils.isBlank(request.toString())) {
+			throw new RuntimeException("null");
+		}
+
+		HttpResponse<String> response;
+		try {
+			response = weatherClient.getCurrentWeather();
+		} catch (URISyntaxException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+
+
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("statusCode", response.statusCode());
+		resultMap.put("body", response.body());
+
+		return resultMap;
 	}
 }
